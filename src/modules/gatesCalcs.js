3514b732673bf469bdd1be7a9f31d71cad0fa491
Vlad Galcev <Vladgalcev03@gmail.com>
2022-09-24 12:48:48 +0300

    set variables for calculations

--- src/modules/gatesCalcs.js
+++ src/modules/gatesCalcs.js
@@ -3,14 +3,15 @@ const { pipeTable } = selectors;
 
 export const gatesCalcs = (quantity) => {
   const tBody = pipeTable.children[1];
-  const pipeLength = 72;
+  const oneGatePipeLength = 72;
+  const totalPipeLength = quantity * oneGatePipeLength;
 
   if (quantity && quantity > 0) {
     tBody.insertAdjacentHTML('beforeend', `
       <tr>
         <th scope="row">Труба ворота</th>
         <td class='td-pcs-gates'>${quantity} шт</td>
-        <td class='td-m-gates'>${quantity * pipeLength} м</td>
+        <td class='td-m-gates'>${totalPipeLength} м</td>
       </tr>
     `);
   }
